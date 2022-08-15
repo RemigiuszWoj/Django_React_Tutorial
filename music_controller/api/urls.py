f34c6a5177152a56dd91f91372a635b17516fcb8
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2022-08-15 12:49:31 +0200

    post

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -4,3 +4,11 @@ from.views import RoomView
 urlpatterns = [
     path('room', RoomView.as_view()),
 ]
+
+from django.urls import path
+from .views import RoomView, CreateRoomViev
+
+urlpatterns = [
+    path('room', RoomView.as_view()),
+    path('create-room', CreateRoomViev.as_view()),
+    ]
